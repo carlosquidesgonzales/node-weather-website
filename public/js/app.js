479eda41621870f0745d567d9560114903ad4d93
Carlos Gonzales <carlos.gonzales.spo17@ecusolna.onmicrosoft.com>
2020-01-08 11:17:33 +0100

    Removed console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("Clieant side javaScript")
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
