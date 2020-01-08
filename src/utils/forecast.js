369d46168b711236676175fa9defe6caccbbebd4
Carlos Gonzales <carlos.gonzales.spo17@ecusolna.onmicrosoft.com>
2020-01-08 12:57:06 +0100

    Update forecast file second time

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) =>{
         else if (body.error) {
             callback('Unable to find location', undefined)
         } else {      
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The lowest temperatire is ' + body.currently.apparentTemperature + '.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The lowest temperatire is ' + body.currently.apparentTemperature + ' degrees.')
         }       
     })  
 
